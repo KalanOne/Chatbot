0660be2c7e1cb59a8938945330996aa0a800a63c
Alan Garcia Diaz <44252319+KalanOne@users.noreply.github.com>
2025-06-24 11:00:10 -0600

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -264,7 +264,7 @@ const styles = StyleSheet.create({
   },
   header: {
     paddingHorizontal: 20,
-    paddingTop: Platform.OS=="android"? 0 : 40,
+    paddingTop: Platform.OS=="android"? 0 : 50,
     paddingBottom: 15,
     borderBottomLeftRadius: 24,
     borderBottomRightRadius: 24,
