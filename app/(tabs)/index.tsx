3505855c886862f5da1f0e856af21b54a83ced38
Alan Garcia Diaz <44252319+KalanOne@users.noreply.github.com>
2025-06-24 11:12:49 -0600

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -259,7 +259,8 @@ const styles = StyleSheet.create({
   },
   header: {
     paddingHorizontal: 20,
-    paddingTop: Platform.OS=="android"? StatusBar.currentHeight : 50,
+    mmarginTop: Platform.OS=="android"? StatusBar.currentHeight : 0
+    paddingTop: Platform.OS=="android"? 0 : 50,
     paddingBottom: 15,
     borderBottomLeftRadius: 24,
     borderBottomRightRadius: 24,
