ea340e77506438b9b50c7ed152fc6e5d26ab1c44
Alan Garcia Diaz <44252319+KalanOne@users.noreply.github.com>
2025-06-26 14:21:01 -0600

    Updated index.tsx

--- app/(tabs)/discover/index.tsx
+++ app/(tabs)/discover/index.tsx
@@ -141,6 +141,7 @@ export default function DiscoverScreen() {
         </TouchableOpacity>
 
         <View style={styles.topicsGrid}>
+          
           {topics.slice(1).map((topic, index) => (
             <TouchableOpacity
               key={topic.id}
